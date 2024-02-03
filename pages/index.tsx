6413a2de7647f6b816d9d3a6e07c484072f0d243
maceteligolden <maceteligolden@gmail.com>
2024-02-03 16:11:28 +0100

    chore:remove unused code in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,9 @@
-import React, { useEffect } from "react";
-import Router, { useRouter } from "next/router";
-
-import { useDispatch } from "react-redux";
-
-import { useProfile } from "@common/UserHooks";
-
-import { logoutUser } from "Components/slices/thunk";
 
 export default function Index() {
-  const dispatch: any = useDispatch();
-  const router = useRouter()
-  const { userProfile, loading } = useProfile();
 
-  useEffect(() => {
-    if (!userProfile && loading) {
-      dispatch(logoutUser());
-    }
-  }, [userProfile, loading, dispatch]);
+  return (
+    <>
 
-  useEffect(() => {
-    if (!userProfile && loading) {
-      router.push("/auth/login");
-    } else {
-      router.push("/dashboard");
-    }
-  }, [router, loading, userProfile]);
-
+    </>
+  )
 }
