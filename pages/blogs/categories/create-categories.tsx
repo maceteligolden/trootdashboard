e2d59563171845bdc53dcaf2116edd1c57333b69
maceteligolden <maceteligolden@gmail.com>
2024-02-03 20:30:08 +0100

    feat:add blogs pages

---
+++ pages/blogs/categories/create-categories.tsx
@@ -0,0 +1,177 @@
+import React, { ReactElement, useState } from 'react';
+import Head from 'next/head';
+import Layout from '@common/Layout';
+import Breadcrumb from '@common/Breadcrumb';
+import { IBreadCrumb } from '@common/interfaces';
+import { Alert, Button, Col, Form, Row } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as Yup from "yup";
+
+const CreateArticle = () => {
+
+    const [passwordtype, setPasswordtype] = useState<boolean>(true)
+
+    const breadcrumbItems: IBreadCrumb[] = [
+        {
+            label: "Dashboard",
+            isLink: true,
+            link: "/dashboard"
+        },
+        {
+            label: "Articles",
+            isLink: true,
+            link: "/articles"
+        },
+        {
+            label: "Categories",
+            link: "/articles/categories",
+            isLink: true,
+        }, 
+        {
+            label: "Create Article Categories",
+            isLink: false
+        }
+    ];
+
+    const validation: any = useFormik({
+        // enableReinitialize : use this flag when initial values needs to be changed
+        enableReinitialize: true,
+
+        initialValues: {
+            firstname: "",
+            lastname: "",
+            email: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            email: Yup.string().required("Please Enter Your Email"),
+            password: Yup.string().required("Please Enter Your Password"),
+            firstname: Yup.string().required("Please Enter Your Firstname"),
+            lastname: Yup.string().required("Please Enter Your Lastname")
+        }),
+        onSubmit: (values) => {
+         
+        }
+    });
+
+    return (
+        <React.Fragment>
+            <Breadcrumb pageName="Create Article Category" items={breadcrumbItems}/>
+            <Row className="px-0">
+                <Col lg={5}>
+                    {false && true ? (<Alert variant="danger"> error message goes here </Alert>) : null}
+                    <div className=" mt-5">
+                        <Form
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                validation.handleSubmit();
+                                return false;
+                            }}
+                        >
+
+                            <div className="mb-3">
+                                <Form.Label htmlFor="firstname" className="form-label">FirstName</Form.Label>
+                                <Form.Control className="form-control" id="firstname" placeholder="Enter firstname"
+                                    name="firstname"
+                                    type="text"
+                                    onChange={validation.handleChange}
+                                    onBlur={validation.handleBlur}
+                                    value={validation.values.firstname || ""}
+                                    isInvalid={
+                                        validation.touched.firstname && validation.errors.firstname ? true : false
+                                    }
+
+                                />
+                                {validation.touched.firstname && validation.errors.firstname ? (
+                                    <Form.Control.Feedback type="invalid">{validation.errors.firstname}</Form.Control.Feedback>
+                                ) : null}
+                            </div>
+
+                            <div className="mb-3">
+                                <Form.Label htmlFor="lastname" className="form-label">LastName</Form.Label>
+                                <Form.Control className="form-control" id="lastname" placeholder="Enter lastname"
+                                    name="lastname"
+                                    type="text"
+                                    onChange={validation.handleChange}
+                                    onBlur={validation.handleBlur}
+                                    value={validation.values.lastname || ""}
+                                    isInvalid={
+                                        validation.touched.lastname && validation.errors.lastname ? true : false
+                                    }
+
+                                />
+                                {validation.touched.lastname && validation.errors.lastname ? (
+                                    <Form.Control.Feedback type="invalid">{validation.errors.lastname}</Form.Control.Feedback>
+                                ) : null}
+                            </div>
+
+                            <div className="mb-3">
+                                <Form.Label htmlFor="email" className="form-label">Email</Form.Label>
+                                <Form.Control className="form-control" id="email" placeholder="Enter email"
+                                    name="email"
+                                    type="email"
+                                    onChange={validation.handleChange}
+                                    onBlur={validation.handleBlur}
+                                    value={validation.values.email || ""}
+                                    isInvalid={
+                                        validation.touched.email && validation.errors.email ? true : false
+                                    }
+
+                                />
+                                {validation.touched.email && validation.errors.email ? (
+                                    <Form.Control.Feedback type="invalid">{validation.errors.email}</Form.Control.Feedback>
+                                ) : null}
+                            </div>
+
+                            <div className="mb-3">
+                            
+                                <Form.Label className="form-label" htmlFor="password-input">Password</Form.Label>
+                                <div className="position-relative auth-pass-inputgroup mb-3">
+                                    <Form.Control type={passwordtype ? "password" : "text"} className="form-control pe-5 password-input" placeholder="Enter password" id="password-input"
+                                        name="password"
+                                        value={validation.values.password || ""}
+                                        onChange={validation.handleChange}
+                                        onBlur={validation.handleBlur}
+                                        isInvalid={
+                                            validation.touched.password && validation.errors.password ? true : false
+                                        }
+                                    />
+                                    {validation.touched.password && validation.errors.password ? (
+                                        <Form.Control.Feedback type="invalid">{validation.errors.password}</Form.Control.Feedback>
+                                    ) : null}
+                                    <Button variant='link' className="position-absolute end-0 top-0 text-decoration-none text-muted password-addon" type="button" id="password-addon" onClick={() => setPasswordtype(!passwordtype)}><i className="ri-eye-fill align-middle"></i></Button>
+                                </div>
+                            </div>
+
+
+                            <div className="mt-4">
+                                <Button variant="primary" className="w-100" type="submit">
+                                    {/* {error || loading ? <Spinner animation="border" size="sm" className="me-2"></Spinner> : null} */}
+                                    Create Account
+                                </Button>
+                            </div>
+
+                        
+                        </Form>
+
+                    
+                    </div>
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
+}
+
+CreateArticle.getLayout = (page: ReactElement) => {
+    return (
+      <Layout>
+        <Head>
+                <title>Create Article | Trootfindr</title>
+        </Head>
+        {page}
+      </Layout>
+    )
+};
+
+
+export default CreateArticle;
--- pages/blogs/categories/index.tsx
+++ pages/blogs/categories/index.tsx
@@ -1,24 +1,125 @@
 import React, { ReactElement } from 'react';
 import Head from 'next/head';
 import Layout from '@common/Layout';
+import { IBreadCrumb } from '@common/interfaces';
+import { useRouter } from 'next/router';
+import { Button, Table } from 'react-bootstrap';
+import Taskbar from '@common/Taskbar';
+import Breadcrumb from '@common/Breadcrumb';
+import Link from 'next/link';
 
-const BlogCategories = () => {
+const ArticlesCategories = () => {
+    const router = useRouter();
+
+    const breadcrumbItems: IBreadCrumb[] = [
+        {
+            label: "Dashboard",
+            isLink: true,
+            link: "/dashboard"
+        },
+        {
+            label: "Articles",
+            isLink: true,
+            link: "/dashboard/articles"
+        },
+        {
+            label: "Categories",
+            isLink: false,
+        }
+    ]
+
     return (
         <React.Fragment>
-            <Head>
-                <title>BlogCategories | Trootfindr</title>
-            </Head>
+            <Breadcrumb pageName="Articles Category" items={breadcrumbItems}/>
+            <Taskbar>
+                <Button variant="primary" className="w-10" onClick={() => {
+                    router.push("/articles/categories/create-categories")
+                }}>
+                    Add Categories
+                </Button>
+            </Taskbar>
+            <div className="table-responsive">
+                <Table className="table-borderless align-middle table-nowrap mb-0">
+                    <thead>
+                        <tr>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Job Title</th>
+                            <th scope="col">Date</th>
+                            <th scope="col">Status</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td className="fw-medium">01</td>
+                            <td>Annette Black</td>
+                            <td>Industrial Designer</td>
+                            <td>10, Nov 2021</td>
+                            <td><span className="badge badge-soft-success">Active</span></td>
+                            <td>
+                                <div className="hstack gap-3 fs-15">
+                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
+                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="fw-medium">02</td>
+                            <td>Bessie Cooper</td>
+                            <td>Graphic Designer</td>
+                            <td>13, Nov 2021</td>
+                            <td><span className="badge badge-soft-success">Active</span></td>
+                            <td>
+                                <div className="hstack gap-3 fs-15">
+                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
+                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="fw-medium">03</td>
+                            <td>Leslie Alexander</td>
+                            <td>Product Manager</td>
+                            <td>17, Nov 2021</td>
+                            <td><span className="badge badge-soft-success">Active</span></td>
+                            <td>
+                                <div className="hstack gap-3 fs-15">
+                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
+                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="fw-medium">04</td>
+                            <td>Lenora Sandoval</td>
+                            <td>Applications Engineer</td>
+                            <td>25, Nov 2021</td>
+                            <td><span className="badge badge-soft-danger">Disabled</span></td>
+                            <td>
+                                <div className="hstack gap-3 fs-15">
+                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
+                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
         </React.Fragment>
     );
 }
 
-BlogCategories.getLayout = (page: ReactElement) => {
+ArticlesCategories.getLayout = (page: ReactElement) => {
     return (
       <Layout>
+            <Head>
+                <title>ArticlesCategories | Trootfindr</title>
+            </Head>
         {page}
       </Layout>
     )
 };
 
 
-export default BlogCategories;
+export default ArticlesCategories;
