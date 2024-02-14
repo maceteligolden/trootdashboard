623aab9277aad611df4100d1ffa2df2c79828449
maceteligolden <maceteligolden@gmail.com>
2024-02-14 17:12:13 +0100

    feat:add blog model

---
+++ lib/models/blog.model.ts
@@ -0,0 +1,8 @@
+import { Base } from "./base";
+import { Category } from "./category.model";
+
+export interface Blog extends Base {
+    title?: string;
+    content?: string;
+    category?: string | Category;
+}
