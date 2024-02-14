4fe77c995c17c3f8dc34dc2d74a355dd3a242480
maceteligolden <maceteligolden@gmail.com>
2024-02-14 17:15:45 +0100

    feat:add blogs categories api

--- pages/blogs/categories/index.tsx
+++ pages/blogs/categories/index.tsx
@@ -1,18 +1,35 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import Head from 'next/head';
 import Layout from '@common/Layout';
 import { IBreadCrumb } from '@common/interfaces';
 import { useRouter } from 'next/router';
-import { Button, Table } from 'react-bootstrap';
+import { Alert, Button, Table } from 'react-bootstrap';
 import Taskbar from '@common/Taskbar';
 import Breadcrumb from '@common/Breadcrumb';
 import Link from 'next/link';
-import { GetServerSideProps } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import cookie from "cookie";
 import { pageRoutes } from 'lib/constants';
+import { Category, CategoryTypes } from 'lib/models/category.model';
+import { deleteCategory, getCategory } from 'lib/services/category.service';
+import { formatDate } from 'lib/utils/formatDate';
+import DeleteModal from '@common/modals/DeleteModal';
 
-const Categories = () => {
+const Categories = ({
+    blogCategories
+  }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
     const router = useRouter();
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [ isDeleted, setIsDeleted] = useState<boolean>(false);
+    const [ selectedId, setSelectedId] = useState<string>("");
+    const [ selectedName, setSelectedName ] = useState<string>("");
+
+    const handleClose = () => setShowDeleteModal(false);
+    const handleShow = (id: string, name: string) => {
+        setSelectedId(id);
+        setSelectedName(name);
+        setShowDeleteModal(true);
+    }
 
     const breadcrumbItems: IBreadCrumb[] = [
         {
@@ -29,11 +46,26 @@ const Categories = () => {
             label: "Categories",
             isLink: false,
         }
-    ]
+    ];
+
+    const handleDelete = async () => {
+        try{
+            setIsDeleted(false);
+            handleClose();
+            await deleteCategory(selectedId);
+    
+            setIsDeleted(true);
+    
+            router.reload();
+        } catch(err){
+            
+        }
+       }
 
     return (
         <React.Fragment>
             <Breadcrumb pageName="Blog Category" items={breadcrumbItems}/>
+            {isDeleted && isDeleted? (<Alert variant="danger"> Successfully deleted category </Alert>) : null}
             <Taskbar>
                 <Button variant="primary" className="w-10" onClick={() => {
                     router.push("/blogs/categories/create-categories")
@@ -45,69 +77,41 @@ const Categories = () => {
                 <Table className="table-borderless align-middle table-nowrap mb-0">
                     <thead>
                         <tr>
-                            <th scope="col">ID</th>
                             <th scope="col">Name</th>
-                            <th scope="col">Job Title</th>
-                            <th scope="col">Date</th>
-                            <th scope="col">Status</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Date Created</th>
+                            <th scope="col">Date Updated</th>
                             <th scope="col"></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="fw-medium">01</td>
-                            <td>Annette Black</td>
-                            <td>Industrial Designer</td>
-                            <td>10, Nov 2021</td>
-                            <td><span className="badge badge-soft-success">Active</span></td>
-                            <td>
-                                <div className="hstack gap-3 fs-15">
-                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
-                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="fw-medium">02</td>
-                            <td>Bessie Cooper</td>
-                            <td>Graphic Designer</td>
-                            <td>13, Nov 2021</td>
-                            <td><span className="badge badge-soft-success">Active</span></td>
-                            <td>
-                                <div className="hstack gap-3 fs-15">
-                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
-                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="fw-medium">03</td>
-                            <td>Leslie Alexander</td>
-                            <td>Product Manager</td>
-                            <td>17, Nov 2021</td>
-                            <td><span className="badge badge-soft-success">Active</span></td>
-                            <td>
-                                <div className="hstack gap-3 fs-15">
-                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
-                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td className="fw-medium">04</td>
-                            <td>Lenora Sandoval</td>
-                            <td>Applications Engineer</td>
-                            <td>25, Nov 2021</td>
-                            <td><span className="badge badge-soft-danger">Disabled</span></td>
-                            <td>
-                                <div className="hstack gap-3 fs-15">
-                                    <Link href="#" className="link-primary"><i className="ri-settings-4-line"></i></Link>
-                                    <Link href="#" className="link-danger"><i className="ri-delete-bin-5-line"></i></Link>
-                                </div>
-                            </td>
-                        </tr>
+                        {blogCategories && blogCategories.map((category: Category)=> {
+                            return (
+                                <>
+                                    <tr>
+                                        <td className="fw-medium">{category.name}</td>
+                                        <td>{category.description}</td>
+                                        <td>{formatDate(category.created_at)}</td>
+                                        <td>{formatDate(category.updated_at)}</td>
+                                        <td>
+                                            <div className="hstack gap-3 fs-15">
+                                                <Link href={`/blogs/categories/update-categories?categoryId=${category._id}&&name=${category.name}&&description=${category.description}`}><i className="ri-edit-line link-primary"></i></Link>
+                                                <i className="ri-delete-bin-5-line link-danger" onClick={() => handleShow(category._id, category.name)}></i>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                   
+                                </>
+                            )})}
                     </tbody>
                 </Table>
+                <DeleteModal 
+                    item={selectedName} 
+                    onDelete={handleDelete} 
+                    onClose={handleClose} 
+                    status={showDeleteModal}
+                />
             </div>
         </React.Fragment>
     );
@@ -126,18 +130,19 @@ Categories.getLayout = (page: ReactElement) => {
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const session = cookie.parse(context.req.headers.cookie || '');
- 
+    const blogCategories = await getCategory(CategoryTypes.BLOG);
+
     if (!session['token']) {
       return {
         redirect: {
-          destination: pageRoutes.auth.login, // Redirect to login page if not authenticated
+          destination: pageRoutes.auth.login, 
           permanent: false,
         },
       };
     }
   
     return {
-      props: {},
+      props: {blogCategories},
     };
   };
 
