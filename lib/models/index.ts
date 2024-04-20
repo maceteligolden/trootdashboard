7361765c74026372ad17bff6636fe9d79d3e9cbe
maceteligolden <maceteligolden@gmail.com>
2024-04-20 22:32:23 +0100

    update:export tags model

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -3,11 +3,13 @@ import { Article } from "./article.model";
 import { Blog } from "./blog.model";
 import { Category } from "./category.model";
 import { Order } from "./order.model";
+import { Tag } from "./tag.model";
 
 export type {
     Category,
     Blog,
     Account,
     Article,
-    Order
+    Order,
+    Tag
 }
